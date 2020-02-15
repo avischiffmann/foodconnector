4c9d009611e7622d31bc72f39ef4ed5f53f22dae
Avi Schiffmann <avischiffmann@gmail.com>
2020-02-15 15:58:07 -0800

    database and map

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ app.get('/', async (req, res) => {
 
 app.listen(process.env.PORT || 3000);
 console.log("Listening on port: " + 3000);
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://foodconnector-c2f13.firebaseio.com"
+});
+
+let db = admin.firestore();
+
+
+let docRef = db.collection('users').doc('qian');
+
+let setAda = docRef.set({
+  first: 'qian',
+  last: 'zuo'
+});
